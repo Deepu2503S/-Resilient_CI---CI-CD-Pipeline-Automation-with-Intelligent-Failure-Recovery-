654c349fe44a980d01296c356373f0b62e5a7333
Anupam Tripathi <tripathi.anupam1201@gmail.com>
2026-03-09 10:04:50 +0530

    Updated Notification service

--- modules/notificationService.js
+++ modules/notificationService.js
@@ -1,8 +1,26 @@
+import nodemailer from "nodemailer"
 
 export default class NotificationService{
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            service : "gmail",
+            auth : {
+                user : process.env.EMAIL_USER,
+                pass : process.env.EMAIL_PASS
 
-    sendNotification(notification){
-        console.log("Notification sent : ",notification);
+            }
+        });
     }
+async sendNotification(message,userEmail){
+    const reciver = userEmail || process.env.EMAIL_TO;
+    await this.transporter.sendMail({
+        from : process.env.EMAIL_USER,
+        to : reciver,
+        subject : "CI/CD Pipeline Notification",
+        text : message
+    })
+
+}
+
 
 }
