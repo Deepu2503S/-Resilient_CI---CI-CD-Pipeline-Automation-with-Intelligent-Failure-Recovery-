09dfcdd7802b2c27e0443e01ef1555c236591906
Anupam Tripathi <tripathi.anupam1201@gmail.com>
2026-03-23 10:11:58 +0530

    added bll

--- src/main.js
+++ src/main.js
@@ -5,46 +5,32 @@ import NotificationService from "../modules/notificationService.js";
 import Logger from "../modules/logger.js";
 import MonitoringService from "../modules/monitoringService.js";
 import dotenv from "dotenv";
-import { savePipelineRun, saveLog } from "./db/dbService.js";
+import executePipeline from "./bll/pipelineManager.js";
 
 dotenv.config();
+import executePipeline from "./bll/pipelineManager.js";
 
 
-const pipeline = new CICDExecution("app.js")
-const classsifier = new FailureClassifier()
-const recovery = new RecoverManager()
-const logger = new Logger()
-const notification = new NotificationService()
-const monitor = new MonitoringService()
-
-
+const dep = {
+    pipeline : new CICDExecution(app.py),
+    classifier : new FailureClassifier(),
+    recovery : new RecoverManager(),
+    notification : new NotificationService(),
+    logger : new Logger(),
+    monitor : new MonitoringService()
+}
 
 export default async function startPipeline(){
-    logger.log("Pipeline started..")
-    monitor.trackStatus("RUNNING")
-    saveLog("Pipeline started");
-
-    const result = await pipeline.runPipeline()
-
-    if(result.status==="FAILURE"){
-        const failureType = classsifier.classify(result.error)
-        monitor.trackStatus("FAILED")
-        logger.log(`Failure detected ${failureType}`)
-        const recoveryAction = recovery.recover(failureType);
-        logger.log(`Recovery action : ${recoveryAction}`)
-        savePipelineRun("FAILURE", failureType, recoveryAction);
-
-       await notification.sendNotification(`Pipeline failed due to ${failureType}`)
-        logger.log(`Notification sent for failure detection of ${failureType}`)
-
+    try {
+        
+        const result = executePipeline(dep);
+        return result;
+    } catch (error) {
+        console.log("Pipeline Crash",error.message);
+        return {
+            status : "ERROR",
+            error : error.message
+        }
+        
     }
-    else{
-        monitor.trackStatus("SUCCESS")
-        logger.log("Pipeline Successfull")
-        await notification.sendNotification("Pipeline executed successfully");
-        logger.log("Notification of pipeline success sent!")
-        savePipelineRun("SUCCESS", null, null);
-    }
-
-    
 }
